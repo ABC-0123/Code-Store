9e8d4a940e20b17e6ea5fcf2425889c035eb0e7b
666greatChina888 <101241010+666greatChina888@users.noreply.github.com>
2022-04-12 19:39:54 +0800

    Add files via upload

---
+++ utils/get_clash.py
@@ -0,0 +1,22 @@
+import os
+import time
+import shutil
+from utils.yamlUtils import YamlUtils
+from utils.cfmem import get_content as cfmem_content
+
+changfengoss = os.path.join("changfengoss")
+dirname = time.strftime("%Y_%m_%d", time.localtime(time.time()))
+yamlUtils = YamlUtils(changfengoss)
+yamlUtils.clone_repo("https://ghproxy.com/https://github.com/changfengoss/pub.git")
+yamlUtils.make_template_dict("yaml", dirname)
+yamlUtils.save_file("sub/changfengoss.yaml")
+shutil.rmtree(changfengoss)
+
+cfmem_content()
+
+sub = os.path.join("sub")
+yamlUtils = YamlUtils(sub)
+yamlUtils.make_template(
+    ["cfmem.yaml"]
+)
+yamlUtils.save_file("sub/combine.yaml")
